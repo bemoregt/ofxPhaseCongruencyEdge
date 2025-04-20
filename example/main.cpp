5cf25dddb8834b012839681625c6eeefe7804ba3
Gromit Park <bemore.one@gmail.com>
2025-04-20 14:50:40 +0900

    Add example main file

--- example/main.cpp
+++ example/main.cpp
@@ -3,11 +3,12 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1536, 512, OF_WINDOW);			// <-------- setup the GL context
-
+	ofSetupOpenGL(1536, 512, OF_WINDOW);	// <-------- setup the GL context
+    
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
+    ofSetWindowTitle("Phase Congruency Edge and Corner Detector");
 	ofRunApp(new ofApp());
 
 }
